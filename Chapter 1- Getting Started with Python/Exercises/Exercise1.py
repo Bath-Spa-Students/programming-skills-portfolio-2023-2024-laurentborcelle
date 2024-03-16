04030c7b853af90326ac2c9cc7765cf6136e4a7a
laurentborcelle <144213411+laurentborcelle@users.noreply.github.com>
2024-03-16 08:24:52 +0300

    Exercise 1.py

--- Chapter 1- Getting Started with Python/Exercises/Exercise1.py
+++ Chapter 1- Getting Started with Python/Exercises/Exercise1.py
@@ -6,3 +6,23 @@ Twinkle, twinkle, little star,
 Twinkle, twinkle, little star, 
 	How I wonder what you are
        ''')
+print ("Hello World")
+# is used for single line comments. i am going to print some welcome speech
+print ("Welcome to Programming")
+print ('''Welcome to BathSpa
+Welcome to Programming''')
+
+# Name of the students
+print("Sofia Lorin Borelo")
+print("Hyacinth Ava Toribio")
+print ('''Twinkle twinkle little star
+       how I wonder what you are?
+       up above the world so high''')
+
+
+# Print 5 Names of your Friends
+print ("Shaima Francisco")
+print ("Hyacinth Ava Toribio")
+print ("Jniel Louise Bulaong")
+print ("Janine Rye Gatdula")
+print ("Naomi")
