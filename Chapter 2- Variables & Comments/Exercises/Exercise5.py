bc4d2c03ea1b71412a6834371770d4ff3145cdca
laurentborcelle <144213411+laurentborcelle@users.noreply.github.com>
2024-03-16 08:26:22 +0300

    Exercise 5.py

---
+++ Chapter 2- Variables & Comments/Exercises/Exercise5.py
@@ -0,0 +1,9 @@
+usbstick_cost = 6
+budget = 50
+number_usb_sticks = budget//usbstick_cost
+remaining_budget = budget%usbstick_cost
+
+print ("The girl can buy", number_usb_sticks, "USB sticks.")
+print ("She will have £",remaining_budget, "left.")
+
+
