b33eb6bf0f191aef2bb6ad5ac3ba6b18b49b147e
laurentborcelle <144213411+laurentborcelle@users.noreply.github.com>
2024-04-02 08:35:19 +0300

    Exercise 2.py

--- Chapter 2- Variables & Comments/Exercises/Exercise2.py
+++ Chapter 2- Variables & Comments/Exercises/Exercise2.py
@@ -1,1 +1,3 @@
-print('Haley Dunphy said, "No one wants to think at graduation! It’s graduation, the time when we celebrate being done with thinking."') 
+## Exercise 2: Variables
+
+print ('Haley Dunphy claims, "No one wants to think at graduation! It’s graduation, the time when we celebrate being done with thinking."') 
