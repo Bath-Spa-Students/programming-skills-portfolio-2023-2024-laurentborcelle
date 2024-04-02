cf8e92c7b4a2c51babbb88011ab5d12c68a00ab1
laurentborcelle <144213411+laurentborcelle@users.noreply.github.com>
2024-04-02 16:05:43 +0300

    Exercise 1.py

---
+++ Chapter 7- Functions/Exercises/Exercise1.py
@@ -0,0 +1,7 @@
+## Exercise 1:   Message 
+
+def display_message():
+    message = "Within this chapter, I am learning about ________________________________."
+    print (message)
+
+display_message()
