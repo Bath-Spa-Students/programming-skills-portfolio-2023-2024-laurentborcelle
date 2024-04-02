a49be1a4a3471c5407ece782e5f8573999ca1272
laurentborcelle <144213411+laurentborcelle@users.noreply.github.com>
2024-04-02 08:38:17 +0300

    Exercise 4.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise4.py
@@ -0,0 +1,18 @@
+## Exercise 4: Guest List
+
+myguest_list = ['Byredo', 'Carolina Herrera', 'Jo Malone', 'Maison Margiela', 'Narciso Rodriguez']
+
+name = myguest_list[0].title()
+print (name + ", we are most delighted to invite you over for dinner.\n")
+
+name = myguest_list[1].title()
+print (name + ", we are most delighted to invite you over for dinner.\n")
+
+name = myguest_list[2].title()
+print (name + ", we are most delighted to invite you over for dinner.\n")
+
+name = myguest_list[3].title()
+print (name + ", we are most delighted to invite you over for dinner.\n")
+
+name = myguest_list[4].title()
+print (name + ", we are most delighted to invite you over for dinner.\n")
