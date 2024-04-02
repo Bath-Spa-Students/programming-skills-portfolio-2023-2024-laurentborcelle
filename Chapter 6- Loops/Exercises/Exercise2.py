2b279416f721867e3fa3d2cef0f16a5ed4e9e751
laurentborcelle <144213411+laurentborcelle@users.noreply.github.com>
2024-04-02 08:43:20 +0300

    Exercise 2.py

---
+++ Chapter 6- Loops/Exercises/Exercise2.py
@@ -0,0 +1,21 @@
+# Exercise 2: Movie Tickets
+
+# Prompt customer instructions
+prompt = "\nEnter your age."
+
+prompt += "\nEnter [quit] when you are finished: "
+
+# Loop execution
+while True:
+    
+    customer_age = input(prompt)
+    if customer_age == 'quit':
+        break
+    customer_age = int(customer_age)
+
+    if customer_age < 3:
+        print ("Your ticket is free.")
+    elif customer_age < 13:
+        print ("The ticket is at the cost of $10.")
+    else:
+        print ("The ticket is at the cost of $15.")
