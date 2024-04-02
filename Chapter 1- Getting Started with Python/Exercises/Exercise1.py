6122d49c852e4e98d2959ec4bc722b71acc53a5d
laurentborcelle <144213411+laurentborcelle@users.noreply.github.com>
2024-04-02 08:33:00 +0300

    Exercise 1.py

--- Chapter 1- Getting Started with Python/Exercises/Exercise1.py
+++ Chapter 1- Getting Started with Python/Exercises/Exercise1.py
@@ -1,3 +1,5 @@
+## Exercise 1: Print Strings
+
 print ('''
 Twinkle, twinkle, little star,
 	How I wonder what you are! 
@@ -6,23 +8,5 @@ Twinkle, twinkle, little star,
 Twinkle, twinkle, little star, 
 	How I wonder what you are
        ''')
-print ("Hello World")
-# is used for single line comments. i am going to print some welcome speech
-print ("Welcome to Programming")
-print ('''Welcome to BathSpa
-Welcome to Programming''')
-
-# Name of the students
-print("Sofia Lorin Borelo")
-print("Hyacinth Ava Toribio")
-print ('''Twinkle twinkle little star
-       how I wonder what you are?
-       up above the world so high''')
 
 
-# Print 5 Names of your Friends
-print ("Shaima Francisco")
-print ("Hyacinth Ava Toribio")
-print ("Jniel Louise Bulaong")
-print ("Janine Rye Gatdula")
-print ("Naomi")
