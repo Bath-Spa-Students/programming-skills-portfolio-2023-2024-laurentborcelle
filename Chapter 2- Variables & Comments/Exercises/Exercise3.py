b8629c64e5d5bd7525b9678c02fb95658993bf09
laurentborcelle <144213411+laurentborcelle@users.noreply.github.com>
2024-04-02 08:35:54 +0300

    Exercise 3.py

--- Chapter 2- Variables & Comments/Exercises/Exercise3.py
+++ Chapter 2- Variables & Comments/Exercises/Exercise3.py
@@ -1,13 +1,21 @@
+## Exercise 3: Stripping Names
+
 name = "\tLaurent Borcelle\n"
 
 print ("Unmodified:")
 print (name)
 
+# lstrip() function
 print ("\nUsing lstrip():")
 print (name.lstrip())
 
+# rstrip() function
 print ("\nUsing rstrip():")
 print (name.rstrip())
 
+# strip() function
 print ("\nUsing strip():")
 print (name.strip())
+
+# Additional line break
+print ("\n")
