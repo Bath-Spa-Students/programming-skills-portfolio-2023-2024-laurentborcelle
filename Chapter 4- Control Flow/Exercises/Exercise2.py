e4c55e54a3e10f8d7e80138c0c5471aa337ff7ed
laurentborcelle <144213411+laurentborcelle@users.noreply.github.com>
2024-04-02 08:40:17 +0300

    Exercise 2.py

---
+++ Chapter 4- Control Flow/Exercises/Exercise2.py
@@ -0,0 +1,9 @@
+# Exercise 2: Alien Colors #2 
+
+
+shotalien_color = input("Enter the color of the alien that had been shot: ")
+
+if shotalien_color == "green":
+    print ("Good one, Player! You just earned 5 points!")
+else:
+    print ("Excellent, Player! You just earned 10 points!")
