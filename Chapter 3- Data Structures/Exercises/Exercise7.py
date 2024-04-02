33e25cf4a389356c78288d9b139d7c6b5216ff59
laurentborcelle <144213411+laurentborcelle@users.noreply.github.com>
2024-04-02 08:38:59 +0300

    Exercise 7.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise7.py
@@ -0,0 +1,49 @@
+# Exercise 7: Seeing the World 
+
+
+mynew_list = ["Atreides", "Harkonnen", "Corino", "Fenring", "Bene Gesserit"]
+
+print ("Original order:")
+print (mynew_list)
+
+
+mynew_list.sort()
+print ("\nAlphabetical:")
+print (mynew_list)
+
+mynew_list = ["Atreides", "Harkonnen", "Corino", "Fenring", "Bene Gesserit"]
+print ("\n\n\nOriginal order:")
+print (mynew_list)
+
+
+print ("\nReverse Alphabetical:")
+print (sorted(mynew_list, reverse = True))
+
+
+print ("\n\n\nOriginal order:")
+print (mynew_list)
+
+
+
+mynew_list.reverse()
+print("\nReversed:")
+print (mynew_list)
+
+mynew_list.reverse()
+print("\nReversed:")
+print (mynew_list)
+
+
+print ("\n\n\nOriginal order:")
+mynew_list.reverse()
+print (mynew_list)
+
+
+mynew_list.sort()
+print ("\nAlphabetical:")
+print (mynew_list)
+
+
+
+print ("\nReverse Alphabetical:")
+print (sorted(mynew_list, reverse = True))
