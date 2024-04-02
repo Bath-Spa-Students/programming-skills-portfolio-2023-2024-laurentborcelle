9faee5523306996319edbeb4341233c796e18d35
laurentborcelle <144213411+laurentborcelle@users.noreply.github.com>
2024-04-02 08:33:33 +0300

    Exercise 3.py

--- Chapter 1- Getting Started with Python/Exercises/Exercise3.py
+++ Chapter 1- Getting Started with Python/Exercises/Exercise3.py
@@ -1,9 +1,13 @@
+## Exercise 3: Print Date and Time 
+
+# DATE
 from datetime import date
 today = (date.today())
-d = today.strftime("%B %d, %Y")
-print(d)
+d = today.strftime ("%B %d, %Y")
+print ("DATE TODAY:", d)
 
+# TIME
 from datetime import datetime
 now = datetime.now()
-t = now.strftime("%H:%M:%S")
-print(t)
+t = now.strftime ("%H:%M:%S")
+print ("CURRENT TIME:", t)
