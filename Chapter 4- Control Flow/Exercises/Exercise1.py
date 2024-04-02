295eaf2b0503e0a5f633de0c2620ea60fc94d536
laurentborcelle <144213411+laurentborcelle@users.noreply.github.com>
2024-04-02 08:40:07 +0300

    Exercise 1.py

---
+++ Chapter 4- Control Flow/Exercises/Exercise1.py
@@ -0,0 +1,12 @@
+## Exercise 1:  Alien Colors #1
+
+
+# Successful if test
+alien_color = "green"
+if alien_color == "green":
+    print ("Well done, Player! You just earned 5 points!")
+
+# Failure on if test
+alien_color = "yellow"
+if alien_color == "green":
+    print ("Well done, Player! You just earned 5 points!")
