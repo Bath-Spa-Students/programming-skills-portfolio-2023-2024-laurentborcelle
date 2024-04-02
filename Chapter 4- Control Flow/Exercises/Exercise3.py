4eed60002bde895df8e53ab53a4f5b6f614f7aee
laurentborcelle <144213411+laurentborcelle@users.noreply.github.com>
2024-04-02 08:41:22 +0300

    Exercise 3.py

---
+++ Chapter 4- Control Flow/Exercises/Exercise3.py
@@ -0,0 +1,33 @@
+## Exercise 3: Alien Colors #3
+
+# Player has shot a green alien
+shotalien_color = "green"
+if shotalien_color == "yellow":
+    print ("\nWell done, Player! You earned 10 points for shooting down a yellow alien!")
+elif shotalien_color == "red":
+    print ("\nExcellent, Player! You earned 15 points for shooting down a red alien!")
+else:
+    print ("\nYou earned 5 points for shooting down a green alien!")
+
+
+# Player has shot a yellow alien
+
+shotalien_color = "yellow"
+if shotalien_color == "green":
+    print ("\nYou earned 5 points for shooting down a green alien!")
+elif shotalien_color == "red":
+    print ("\nExcellent, Player! You earned 15 points for shooting down a red alien!")
+else:
+    print ("\nWell done, Player! You earned 10 points for shooting down a yellow alien!")
+
+
+# Player has shot a red alien
+
+shotalien_color = "red"
+if shotalien_color == "green":
+    print ("\nYou earned 5 points for shooting down a green alien!")
+elif shotalien_color == "yellow":
+    print ("\nWell done, Player! You earned 10 points for shooting down a yellow alien!")
+else:
+    print ("\nExcellent, Player! You earned 15 points for shooting down a red alien!")
+
