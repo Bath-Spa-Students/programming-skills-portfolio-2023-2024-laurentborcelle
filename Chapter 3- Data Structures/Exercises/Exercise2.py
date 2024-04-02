b236854fda90dce703dbac59eb832ee1ce0403df
laurentborcelle <144213411+laurentborcelle@users.noreply.github.com>
2024-04-02 08:37:40 +0300

    Exercise 2.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise2.py
@@ -0,0 +1,23 @@
+## Exercise 2: Greetings
+
+# List of names
+names = ["Elie", "Saab", "Christian", "Louboutin", "Sandy", "Liang"]
+
+# Greetings personalized with each guest's name
+greeting = "Buongiorno, " + names[0].title() + "."
+print (greeting)
+
+greeting = "Buongiorno, " + names[1].title() + "."
+print (greeting)
+
+greeting = "Buongiorno, " + names[2].title() + "."
+print (greeting)
+
+greeting = "Buongiorno, " + names[3].title() + "."
+print (greeting)
+
+greeting = "Buongiorno, " + names[4].title() + "."
+print (greeting)
+
+message = "Buongiorno, " + names[5].title() + "."
+print(message)
