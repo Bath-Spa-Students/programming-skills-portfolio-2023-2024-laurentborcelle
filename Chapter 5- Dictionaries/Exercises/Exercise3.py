a27068fa8e402c2340725cfb5d3f51f01cb8ed64
laurentborcelle <144213411+laurentborcelle@users.noreply.github.com>
2024-04-02 08:42:35 +0300

    Exercise 3.py

---
+++ Chapter 5- Dictionaries/Exercises/Exercise3.py
@@ -0,0 +1,19 @@
+## Exercise 3: Glossary 2
+
+
+# words: append, elif, looping, nest, repetition, sequence, selection
+
+glossary = {
+    'Append': 'attaches an element to the end of a list.',
+    'Elif': ' is short for "else if" and is used when the first if statement is not true, but with the intention of checking for another condition',
+    'Looping': 'is the method of executing the same block of code repetitively until it meets the given specified condition.',
+    'Nest': 'means to encapsulate and hide functions from external access.',
+    'Repetition': 'are used to repeat the same code multiple times in succession.',
+    'Sequence': 'is an ordered list of instructions.',
+    'Selection': 'is used to select part of a program to be executed based on condition.',
+
+
+}
+
+for word, meaning in glossary.items():
+    print (f"{word}:\n{meaning}\n")
