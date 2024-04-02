facdb706ed50a8d2643414a0fac30cbc1f614d5a
laurentborcelle <144213411+laurentborcelle@users.noreply.github.com>
2024-04-02 16:06:06 +0300

    Exercise 3.py

---
+++ Chapter 7- Functions/Exercises/Exercise3.py
@@ -0,0 +1,12 @@
+## Exercise 3: T-Shirt
+
+# Writing the message function
+def make_shirt(size, msg):
+    print (f"\nI am going to tailor an {size} t-shirt.")
+    print (f'The t-shirt will be printed with, "{msg}"')
+
+# Calling the function of the shirts' sizes and prints
+make_shirt (msg = "Illecebra and Arcanus.", size = 'XS')
+make_shirt ('XL', 'Midas Touch.')
+
+print ("\n")
