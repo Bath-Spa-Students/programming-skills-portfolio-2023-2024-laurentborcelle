eae7d075d8d9a1315bb788d8070047e2adde0c74
laurentborcelle <144213411+laurentborcelle@users.noreply.github.com>
2024-04-02 08:41:37 +0300

    Exercise 4.py

---
+++ Chapter 4- Control Flow/Exercises/Exercise4.py
@@ -0,0 +1,16 @@
+## Exercise 4: Stages of Life
+
+user_age = int(input("Enter age of user: "))
+user_age == 65
+if user_age < 2:
+    print ("This user is a baby.")
+elif user_age < 4:
+    print ("This user is a toddler.")
+elif user_age < 13:
+    print ("This user is a kid.")
+elif user_age < 20:
+    print ("This user is a teenager.")
+elif user_age < 65:
+    print ("This user is an adult.")
+else:
+    print ("This user is an elder.")
