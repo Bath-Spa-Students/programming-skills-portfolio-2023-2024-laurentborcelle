dd02a002722b76674aaf517aeeee61c6fbb08a7a
laurentborcelle <144213411+laurentborcelle@users.noreply.github.com>
2024-04-02 08:37:58 +0300

    Exercise 3.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise3.py
@@ -0,0 +1,9 @@
+## Exercise 3: Your Own List
+
+myfavorite_cars = ["Rolls-Royce Wraith", "BMW 7 Series", "Continental GT", "Range Rover", "Panamera"]
+
+print ("\nThe " + myfavorite_cars[0] + " looks best in white.")
+print ("\nAs designed from the ground up, the "+ myfavorite_cars[1] + " is for particularly demanding target groups in a globalised marketplace.")
+print ("\nMy go-to classiest car on a work day is a "+ myfavorite_cars[2] + ".")
+print ("\nThe best luxury car in the field of SUVs is known to be the " + myfavorite_cars[3] + ".")
+print ("\nMy best friend owns a" + myfavorite_cars[4] + ".")
